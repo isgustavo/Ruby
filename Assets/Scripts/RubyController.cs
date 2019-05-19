d266efe84eca5bed85b8b3f4b1a4ac1eba98bfa2
isgustavo <hi.gustavo.oliveira@gmail.com>
2019-05-19 18:32:11 -0300

    Add initial project

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using odt.util;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = CustomInput.InputBy.GetAxis(Axis.HORIZONTAL);
+        float vertical = CustomInput.InputBy.GetAxis(Axis.VERTICAL);
+        Vector2 position = transform.position;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}
